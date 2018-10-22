1b7d161ddc966a6cf6487a7be1f1a520ae0a9709
philip <liuzhenrain@gmail.com>
2018-10-22 21:05:31 +0700

    更新后台显示内容和数据库

--- sharpknife/mysite/admin.py
+++ sharpknife/mysite/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from .models import AppleAccountModel
+from .models import AppleAccountModel,AccountEventModel
 # Register your models here.
 
+class AccountEventModelInline(admin.TabularInline):
+    '''Tabular Inline View for AccountEventModel'''
+
+    model = AccountEventModel
+    extra = 0
 
 class AppleAccountAdmin(admin.ModelAdmin):
     # fieldsets = [
     #     ('帐号', {"fields":['apple_account']}),
     #     ('是否可用', {"fields":['can_use']}),
     # ]
-    list_display = ('apple_account','small_game','used','create_date')
+    inlines = [AccountEventModelInline]
+    list_display = ('apple_account','small_game','used','status','create_date')
     list_filter = ['used']
 
 
--- sharpknife/mysite/models.py
+++ sharpknife/mysite/models.py
@@ -35,37 +35,57 @@ class AppleAccountModel(models.Model):
     apple_account = models.CharField(max_length=50, unique=True)
     email_pwd = models.CharField(max_length=50)
     apple_pwd = models.CharField(max_length=50)
-    used = models.BooleanField(default=False,verbose_name='是否已使用')
-    game_name = models.CharField(max_length=50, null=True,blank=True)
+    used = models.BooleanField(default=False, verbose_name='是否已使用')
+    game_name = models.CharField(max_length=50, null=True, blank=True)
     vpn_name = models.CharField(
-        max_length=50, blank=True,default='暂未设置',null=True)
+        max_length=50, blank=True, default='暂未设置', null=True)
     account_type = models.CharField(
-        max_length=50, choices=account_type_choice_list, default='QY', null=False,verbose_name='帐号类型')
+        max_length=50, choices=account_type_choice_list, default='QY', null=False, verbose_name='帐号类型')
     use_device = models.CharField(
-        max_length=50, null=True, verbose_name='使用的设备',blank=True)
+        max_length=50, null=True, verbose_name='使用的设备', blank=True)
     upload_date = models.DateField(
-        auto_now=False, auto_now_add=False, verbose_name='上传日期',blank=True,null=True)
+        auto_now=False, auto_now_add=False, verbose_name='上传日期', blank=True, null=True)
     parse_type = models.CharField(
         max_length=50, choices=parse_type_choice_list, default='None', verbose_name='项目处理类型')
     small_game = models.BooleanField(default=False, verbose_name='是否为小游戏')
     status = models.CharField(
-        max_length=200, choices=status_choice_list, default='NONE')
+        max_length=200, choices=status_choice_list, default='NONE',verbose_name='帐号状态')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(
-        'admin'), default='admin', to_field='username',verbose_name='使用者')
-    remarks = models.TextField(null=True, blank=True,verbose_name='备注信息')
-    create_date = models.DateField(auto_now=False, auto_now_add=True,verbose_name='帐号创建时间')
-    
+        'admin'), default='admin', to_field='username', verbose_name='使用者')
+    remarks = models.TextField(null=True, blank=True, verbose_name='备注信息')
+    create_date = models.DateField(
+        auto_now=False, auto_now_add=True, verbose_name='帐号创建时间')
 
     def __str__(self):
         return str(self.apple_account)
 
     def get_queryset(self):
         queryset = self.get_queryset()
-        queryset = queryset # TODO
+        queryset = queryset  # TODO
         return queryset
 
     class Meta():
         verbose_name = "iOS帐号信息"
-        ordering=['used']
+        ordering = ['used']
 
-    
+
+class AccountEventModel(models.Model):
+    """Apple Account Event Message."""
+
+    event_mes = models.CharField(null=False, max_length=1000,verbose_name='事件')
+    event_date = models.DateField(
+        verbose_name='时间发生时间', auto_now=True, auto_now_add=False)
+    account = models.ForeignKey(
+        AppleAccountModel, on_delete=models.CASCADE, related_name='apple_event')
+    who_add = models.ForeignKey(User,related_name='apple_event_who',on_delete=models.CASCADE)
+
+    class Meta:
+        """Meta definition for AccountEventModel."""
+
+        verbose_name = '苹果帐号事件'
+        verbose_name_plural = '苹果帐号事件'
+        ordering = ['-event_date']
+
+    def __str__(self):
+        """Unicode representation of AccountEventModel."""
+        return self.event_mes
